e7021f971b304047b0c82e4c5f1ee3935c12acf9
zuka filfani <zuka.filfani1999@gmail.com>
2026-01-23 13:32:09 +0000

    logo is not showed can you explain why it seems everything normaly

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ export default function Logo({ className }: { className?: string }) {
   return (
     <div className={cn('relative', className)}>
       <Image
-        src="/image/AdtimeLogo.svg"
+        src="/image/AD Time1. new logo.svg"
         alt="SignCraft Logo"
         fill
         className="object-contain"

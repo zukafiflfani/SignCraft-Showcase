953091f0982cda2a348d3f1875ab2a5e9db3a477
zukafilfani <zuka.filfani1999@gmail.com>
2026-04-21 11:06:14 +0400

    update messages and products component

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -34,7 +34,7 @@ export default function Products() {
   ];
 
   return (
-    <section title="caruselImag" className="py-24 sm:py-32 bg-secondary">
+    <section id="products" title="caruselImag" className="py-24 sm:py-32 bg-secondary">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-12">
           <h2 className="font-headline text-4xl sm:text-5xl font-bold text-primary">
